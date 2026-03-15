0b290e60879e6be3a7099b0a74791d0b7a3d6c3a
ibrahimkodehode-png <ibrahim.kodehode@gmail.com>
2026-03-15 22:36:17 +0100

    ready to build an app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial, sans-serif" }}>
+      <h1>Gutendex</h1>
+      <p>My React app is working.</p>
+    </main>
+  );
+}
+
+export default App;
