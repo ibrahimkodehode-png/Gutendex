8eabbb3cb79793fdaa8a7b1826cc7b4155dca545
ibrahimkodehode-png <ibrahim.kodehode@gmail.com>
2026-03-15 23:00:16 +0100

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import HomePage from "./pages/HomePage";
+import CategoryPage from "./pages/CategoryPage";
+import FavoritesPage from "./pages/FavoritesPage";
+import BookDetailsPage from "./pages/BookDetailsPage";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "category/:topic", element: <CategoryPage /> },
+      { path: "favorites", element: <FavoritesPage /> },
+      { path: "book/:id", element: <BookDetailsPage /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
