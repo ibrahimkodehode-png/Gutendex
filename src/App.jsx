563b01d5910b4f883a70d97bedc610cf8e77c23b
ibrahimkodehode-png <ibrahim.kodehode@gmail.com>
2026-03-16 02:41:54 +0100

    app.jsx and index.css REMAKE

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="app-shell">
       <Header />
-      <main className="page-content">
-        <Outlet />
-      </main>
+
+      <div className="app-layout">
+        <Sidebar />
+
+        <main className="page-content main-content">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
